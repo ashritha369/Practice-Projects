4225a98ac7e7292fb71cbd32c0f4cd5be3e01a46
unknown <ashrithagowda666@gmail.com>
2024-02-27 13:03:22 +0530

    calling delete id with endpoint  to backend from client

--- 3.MERN-Stack-CRUD-UserDetails/client/src/screens/Users.jsx
+++ 3.MERN-Stack-CRUD-UserDetails/client/src/screens/Users.jsx
@@ -21,6 +21,14 @@ const Users = () => {
       */
       .catch((err) => console.log(err));
   }, []);
+
+  const handleDelete = (ID) => {
+    // SEND DELETE TO ENDPOINT 5: /deleteUser
+    axios
+      .delete("/deleteUser/" + ID)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -48,7 +56,12 @@ const Users = () => {
                     <Link to={`/edit/${user._id}`} className="btn btn-success">
                       Edit
                     </Link>
-                    <button className="btn btn-sm btn-danger">Delete</button>
+                    <button
+                      className="btn btn-sm btn-danger"
+                      onClick={(e) => handleDelete(user._id)}
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               );
