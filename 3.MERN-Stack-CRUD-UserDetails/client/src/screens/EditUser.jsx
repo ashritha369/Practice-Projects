2cd2e1545fc28a8afb3ff6db9cc81a4a0c3ef94f
unknown <ashrithagowda666@gmail.com>
2024-02-27 12:46:06 +0530

    EDIT USER: secondly  send updated values to backend endpoint using put

--- 3.MERN-Stack-CRUD-UserDetails/client/src/screens/EditUser.jsx
+++ 3.MERN-Stack-CRUD-UserDetails/client/src/screens/EditUser.jsx
@@ -13,11 +13,25 @@ const EditUsers = () => {
     // ENDPOINT 3: GET USER BY ID
     axios
       .get("http://localhost:5000/getUser/" + id)
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+        // update result we got for getting user details by id as default values initially
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setAge(result.data.age);
+      })
       .catch((err) => console.log(err));
   }, []);
   const handleUpdate = (e) => {
-    console.log(e);
+    e.preventDefault();
+    //  SEND(updated values) TO ENDPOINT 4: EDIT USER '/editUser'
+    axios
+      .put("http://localhost:5000/editUser/" + id, { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
@@ -30,6 +44,7 @@ const EditUsers = () => {
               type="text"
               placeholder="Enter Name"
               className="form-control"
+              // default value
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
@@ -40,6 +55,7 @@ const EditUsers = () => {
               type="email"
               placeholder="Enter Email"
               className="form-control"
+              // default value
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -50,6 +66,7 @@ const EditUsers = () => {
               type="text"
               placeholder="Enter Age"
               className="form-control"
+              // default value
               value={age}
               onChange={(e) => setAge(e.target.value)}
             />
